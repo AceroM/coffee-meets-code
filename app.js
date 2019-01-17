d4cba90eabbaa370b619166fdbbc05fdb281f703
Leo <leo297747@gmail.com>
2019-01-17 15:04:38 -0500

    Added express backend

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const { database } = require("./models/index");
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded());
+
+app.use("/routes/api", require("./routes/api"));
+
+database.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+database.sync().then(() => console.log("tables created!"));
+
+app.listen(3000, () => {
+    console.log("App is listening on port 3000");
+});
---
+++ routes/api.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+//const { Hats, Users } = require('../models');
+
+module.exports = router;
