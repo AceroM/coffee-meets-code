305320c3c6eb4355238965631fcd2bbd27b3192f
kaizenc <imgtn159@gmail.com>
2019-01-21 13:54:35 -0500

    Checkpoint; working on cards

--- frontend/src/components/SwipePage.js
+++ frontend/src/components/SwipePage.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Cards, { Card } from 'react-swipe-card'
 
 class SwipePage extends Component {
-    render() {
-        console.log(this.props.data.firstName);
+    constructor(props){
+        super(props);
+        this.state = {
+            other_hackers: [],
+            other_hackers_user: []
+        };
+    }
+    componentDidMount(){
         axios.get('api/hackers/allExcept/' + this.props.data.username)
             .then(response => {
                 let potato = response.data.map(x => x.username);
-                console.log(potato);
-                return (
-                    <div>
-                        HERE R UR POSSIBLE SWIPEZ <br/>
-                        {potato[0]}
-                    </div>
-                );
+                this.setState({
+                    other_hackers: response,
+                    other_hackers_user: potato
+                });
+                console.log(this.state.other_hackers_user);
             });
+    }
+    
+    render() {
+        let lmfao = this.state.other_hackers_user.map(item => 
+            <Card >
+                <h2>{item}</h2>
+            </Card>
+        );
         return (
             <div>
-                HERE R UR POSSIBLE SWIPEZ
+                HERE R UR POSSIBLE SWIPEZ <br/>
+                <Cards>
+                    {lmfao}
+                </Cards>
             </div>
         );
     }
