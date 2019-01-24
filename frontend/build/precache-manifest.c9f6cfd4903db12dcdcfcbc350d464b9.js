ca08ca63dd03a5121f32c537776e084a5efa6597
kaizenc <imgtn159@gmail.com>
2019-01-24 10:39:15 -0500

    built files

---
+++ frontend/build/precache-manifest.c9f6cfd4903db12dcdcfcbc350d464b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a39641f9312826fddb4e",
+    "url": "/static/css/main.45813f98.chunk.css"
+  },
+  {
+    "revision": "a39641f9312826fddb4e",
+    "url": "/static/js/main.a39641f9.chunk.js"
+  },
+  {
+    "revision": "8228a6a5abc6e83d7ba3",
+    "url": "/static/css/1.b1446e60.chunk.css"
+  },
+  {
+    "revision": "8228a6a5abc6e83d7ba3",
+    "url": "/static/js/1.8228a6a5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "545c0a1a03c76e6f05a1e4e74c539933",
+    "url": "/static/media/bagel.545c0a1a.png"
+  },
+  {
+    "revision": "625917f6fd2df463fc421e9e423cf31c",
+    "url": "/index.html"
+  }
+];
